9ac4551b795459732896c891a1f100027305746d
Factoryidiot <rhys.scandlyn@gmail.com>
2024-08-20 18:41:55 +1000

    Fixed incorrect syntax

--- hosts/nixos-qemu/default.nix
+++ hosts/nixos-qemu/default.nix
@@ -6,7 +6,7 @@
     ./hardware-configuration.nix
   ];
 
-  boot,loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking = {
