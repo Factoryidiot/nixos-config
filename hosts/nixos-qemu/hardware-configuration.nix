fce5fadd5797b5259602dafb36470515cf3085bd
Factoryidiot <rhys.scandlyn@gmail.com>
2024-08-21 19:04:00 +1000

    hardware-configuration.nix

---
+++ hosts/nixos-qemu/hardware-configuration.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ "ahci" "sr_mod" "virtio_blk" "virtio_pci" "virtio_scsi" "xhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  swapDevices = [ ];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  # powerManagement.cpuFreqGovernor lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributionFirmware;
+}
