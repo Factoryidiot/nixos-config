cdcaf19ff0327a0f3271f5d4e9f88f90028ae75f
Factoryidiot <rhys.scandlyn@gmail.com>
2024-08-23 21:01:40 +1000

    update hardare-configuration.nix

--- hosts/nixos-qemu/hardware-configuration.nix
+++ hosts/nixos-qemu/hardware-configuration.nix
@@ -17,5 +17,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   # powerManagement.cpuFreqGovernor lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributionFirmware;
+  # hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
