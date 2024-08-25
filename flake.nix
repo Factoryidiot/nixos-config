f97d897967b27ddd802fabb4b132e6b1c753c6a2
Factoryidiot <rhys.scandlyn@gmail.com>
2024-08-25 19:54:41 +1000

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -56,15 +56,15 @@
 
         rhys = home-manager.lib.homeManagerConfiguration {
 
-          # pkgs = 
+          pkgs = nixpkgs.legacyPackages.${system};
 
-#          home-manager.nixosModules.home-manager
- #         {
+          home-manager.nixosModules.home-manager
+         {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = inputs // specialArgs;
             home-manager.users.${username} = import ./users/${username}/home.nix;
-  #        }
+         }
 
        };
       };
