414d9933d0648d922d6044e3f959a44d90b4a102
Factoryidiot <rhys.scandlyn@gmail.com>
2024-09-01 20:31:41 +1000

    updated whio/hardware-configuration.nix

--- hosts/whio/hardware-configuration.nix
+++ hosts/whio/hardware-configuration.nix
@@ -85,6 +85,6 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
