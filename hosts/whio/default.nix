230b7776c3cef3a4a09e37754bbf1c6767d38ab6
Factoryidiot <rhys.scandlyn@gmail.com>
2024-09-11 20:42:19 +1000

    updated hosts/whiw/default.nix [nvidia]

--- hosts/whio/default.nix
+++ hosts/whio/default.nix
@@ -1,18 +1,37 @@
-{ config, pkgs, ... }:
-
-{ 
+{
+  config,
+  pkgs,
+  ...
+}: { 
   imports = [
     ../../modules/configuration.nix
  
     ./hardware-configuration.nix
-
-    # ./impermanence.nix
-    # ./secureboot.nix
   ];
 
   networking = {
     hostName = "whio";
   };
+
+  hardware.graphics = {
+    enable = true;
+    # needed by nvidia-docker
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/nvidia-x11/default.nix
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # required by most wayland compositors!
+    modesetting.enable = true;
+    powerManagement.enable = true;
+  };
+  hardware.nvidia-container-toolkit.enable = true;
+
+  # for Nvidia GPU
+  services.xserver.videoDrivers = [ "nvidia" ]; # will install nvidia-vaapi-driver by default
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -21,4 +40,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
 }
