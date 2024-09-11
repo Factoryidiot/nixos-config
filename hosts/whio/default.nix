4f1394f355c8ab201f7394d557c2c5ea5ce78153
Factoryidiot <rhys.scandlyn@gmail.com>
2024-09-11 20:47:42 +1000

    updated hosts/whiw/default.nix [nvidia]

--- hosts/whio/default.nix
+++ hosts/whio/default.nix
@@ -15,20 +15,20 @@
 
   hardware.graphics = {
     enable = true;
-    # needed by nvidia-docker
-    enable32Bit = true;
+    # enable32Bit = true; # needed by nvidia-docker
   };
 
-  hardware.nvidia = {
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/nvidia-x11/default.nix
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    # required by most wayland compositors!
-    modesetting.enable = true;
-    powerManagement.enable = true;
+  hardware = { 
+    nvidia = {
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/nvidia-x11/default.nix
+      # package = config.boot.kernelPackages.nvidiaPackages.stable;
+      # required by most wayland compositors!
+      modesetting.enable = true;
+      powerManagement.enable = true;
+    };
+    nvidia-container-toolkit.enable = true;
   };
-  hardware.nvidia-container-toolkit.enable = true;
 
   # for Nvidia GPU
   services.xserver.videoDrivers = [ "nvidia" ]; # will install nvidia-vaapi-driver by default
