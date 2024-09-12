41593351a2bf47ee34d5dfc0e9b21d6527947e8c
Factoryidiot <rhys.scandlyn@gmail.com>
2024-09-12 18:29:48 +1000

    updated modules/nvidia.nix

--- hosts/whio/default.nix
+++ hosts/whio/default.nix
@@ -5,33 +5,14 @@
 }: { 
   imports = [
     ../../modules/configuration.nix
- 
+    ../../modules/nvidia.nix
+
     ./hardware-configuration.nix
   ];
 
   networking = {
     hostName = "whio";
   };
-
-  hardware.graphics = {
-    enable = true;
-    # enable32Bit = true; # needed by nvidia-docker
-  };
-
-  hardware = { 
-    nvidia = {
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/nvidia-x11/default.nix
-      # package = config.boot.kernelPackages.nvidiaPackages.stable;
-      # required by most wayland compositors!
-      modesetting.enable = true;
-      powerManagement.enable = true;
-    };
-    nvidia-container-toolkit.enable = true;
-  };
-
-  # for Nvidia GPU
-  services.xserver.videoDrivers = [ "nvidia" ]; # will install nvidia-vaapi-driver by default
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,22 +1,41 @@
 {
-  pkgs,
+  config,
   lib,
+  pkgs,
   ...
 }: {
 
-  hardware.graphics = {
-    enable = true;
-    # enable32Bit = true; # needed by nvidia-docker
-  };
+  hardware = {
+    graphics = {
+      enable = true;
+      # enable32Bit = true; # needed by nvidia-docker
+    };
 
-  hardware = { 
     nvidia = {
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/nvidia-x11/default.nix
       # package = config.boot.kernelPackages.nvidiaPackages.stable;
       # required by most wayland compositors!
       modesetting.enable = true;
-      powerManagement.enable = true;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of 
+      # supported GPUs is at: 
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Enable the Nvidia settings menu,
+	    # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
     };
     nvidia-container-toolkit.enable = true;
   };
