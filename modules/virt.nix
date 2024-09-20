58244aa95c01194461404f829397e99035042a64
Factoryidiot <rhys.scandlyn@gmail.com>
2024-09-20 21:19:50 +1000

    updated modules/virt.nix

--- modules/virt.nix
+++ modules/virt.nix
@@ -2,8 +2,47 @@
   config,
   lib,
   pkgs,
+  username,
   ...
 }: {
 
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+        swtpm.enable = true;
+        swtpm.packages = pkgs.swtpm;
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+
+  users.users.${username}.extraGroups = [ "libvirt" ];
+
+  environment.systemPackages = with pkgs; [
+    libguestfs
+    spice
+    spice-gtk
+    spice-protocol
+    virt-manager
+    virt-viewer
+    virtio-win
+    win-spice
+  ];
+  programs.virt-manager.enable = true;
+
+  home-manager.users.${username} = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+    };
+  };
+
+  systemd.services."libvirtd".reloadIfChanged = true; # reload vm configs from //services/*/libvirt/guests.nix
 
 }
