dfec998be0a449187adccf4943e4cc6b8cb2797a
Factoryidiot <rhys.scandlyn@gmail.com>
2024-09-21 13:55:07 +1000

    updated modules/virt.nix

--- modules/virt.nix
+++ modules/virt.nix
@@ -4,7 +4,57 @@
   pkgs,
   username,
   ...
-}: {
+}:
+let
+  # Geforce RX 4070 Max-Q / Mobile
+  gpuIDs = [
+    "10de:2860"
+    "10de:22bd"
+  ];
+in {
+
+  boot = {
+    initrd.kernelModules = [
+      "vfio_pci"
+      "vfio"
+      "vfio_iommu_type1"
+      "vfio_virqfd"
+
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_uvm"
+      "nvidia_drm"
+    ];
+    kernelParams = [
+      "amd.iommu=on"
+    ] ++ lib.optional cfg.enable ("vfio-pci.ids=" + lib.concatStringsSep "," gpuIDs);
+  };
+
+  environment.systemPackages = with pkgs; [
+    libguestfs
+    spice
+    spice-gtk
+    spice-protocol
+    virt-manager
+    virt-viewer
+    virtio-win
+    win-spice
+  ];
+
+  home-manager.users.${username} = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+    };
+  };
+
+  programs.virt-manager.enable = true;
+
+  systemd.services."libvirtd".reloadIfChanged = true; # reload vm configs from //services/*/libvirt/guests.nix
+
+  users.users.${username}.extraGroups = [ "libvirt" ];
 
   virtualisation = {
     libvirtd = {
@@ -18,31 +68,15 @@
       };
     };
     spiceUSBRedirection.enable = true;
-  };
-
-  users.users.${username}.extraGroups = [ "libvirt" ];
-
-  environment.systemPackages = with pkgs; [
-    libguestfs
-    spice
-    spice-gtk
-    spice-protocol
-    virt-manager
-    virt-viewer
-    virtio-win
-    win-spice
-  ];
-  programs.virt-manager.enable = true;
-
-  home-manager.users.${username} = {
-    dconf.settings = {
-      "org/virt-manager/virt-manager/connections" = {
-        autoconnect = [ "qemu:///system" ];
-        uris = [ "qemu:///system" ];
-      };
+    vfio = {
+      enable = true;
+      IOMMUType = "amd";
+      # devices = [ "10de:1b80" "10de:10f0" ];
+      blacklistNvidia = true;
+      disableEFIfb = false;
+      ignoreMSRs = true;
+      applyACSpatch = false;
     };
   };
-
-  systemd.services."libvirtd".reloadIfChanged = true; # reload vm configs from //services/*/libvirt/guests.nix
 
 }
