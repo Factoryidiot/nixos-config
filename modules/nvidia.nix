6180a2872f336b7cbbc61b1bfcb6824346989685
Factoryidiot <rhys.scandlyn@gmail.com>
2024-09-29 14:06:50 +1000

    updated

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -27,6 +27,10 @@
       # required by most wayland compositors!
       modesetting.enable = true;
 
+      # Enable the Nvidia settings menu,
+	    # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+ 
       # Use the NVidia open source kernel module (not to be confused with the
       # independent third-party "nouveau" open source driver).
       # Support is limited to the Turing and later architectures. Full list of 
@@ -36,28 +40,36 @@
       # Currently alpha-quality/buggy, so false is currently the recommended setting.
       open = false;
 
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
       # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
       # Enable this if you have graphical corruption issues or application crashes after waking
       # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
       # of just the bare essentials.
       powerManagement.enable = true;
-      prime = {
-        reverseSync.enable = true;
-        allowExternalGpu = false;
+      powerManagement.finegrained = true;
 
+      prime = {
         # Make sure to use the correct Bus ID values for your system!
-        nvidiaBusId = "PCI:01:0:0";
         amdgpuBusId = "PCI:65:0:0";
+        nvidiaBusId = "PCI:01:0:0";
+ 
+        offload = {
+          enable = true;
+          enableOffloadCmd =  true; # command: nvidia-offload 
+        };
+        # reverseSync.enable = true;
       };
 
-      # Enable the Nvidia settings menu,
-	    # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
-    };
+   };
 
   };
 
   # for Nvidia GPU
   services.xserver.videoDrivers = [ "nvidia" ]; # will install nvidia-vaapi-driver by default
-
+  settings.nix.unfree = [
+    "nvidia-x11"
+    "nvidia-settings"
+    "nvidia-persistenced"
+  ];
 }
