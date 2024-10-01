c4a91294595698284e6eee1625046d7d03ca5340
Factoryidiot <rhys.scandlyn@gmail.com>
2024-10-01 15:44:09 +1000

    updated modules/nvidia.nix

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -28,7 +28,13 @@ in {
     "nvidia-drm.fbdev=1"
     "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
   ];
-
+  initrd.kernelModules = [
+      "nvidia"
+      "nvidia_drm"
+      "nvidia_modeset"
+      "i2c_nvidia_gpu"
+    ];
+ 
   hardware = {
 
     graphics = {
