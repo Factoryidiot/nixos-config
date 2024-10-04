776991f69635fdc3a772f6a61719ce674b5f189b
Factoryidiot <rhys.scandlyn@gmail.com>
2024-10-05 09:09:18 +1000

    updated home/zsh.nix

--- home/zsh.nix
+++ home/zsh.nix
@@ -13,11 +13,18 @@
       plugins = [ "git" "sudo" ];
     };
     syntaxHighlighting.enable = true;
-    antidote = {
-      enable = true;
-      plugins = [''
-        romkatv/powerlevel10k kind:fpath
-      ''];
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k-config;
+        file = "p10k.zsh";
+      }
+    ];
     };
   };
 
