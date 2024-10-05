ef7cbe56fcaf600c9e525a2dba2ad53a4314067f
Factoryidiot <rhys.scandlyn@gmail.com>
2024-10-05 12:42:55 +1000

    updated home/zsh.nix

--- home/zsh.nix
+++ home/zsh.nix
@@ -26,26 +26,26 @@
     };
     syntaxHighlighting.enable = true;
 
-    zplug = {
-      enable = true;
-      plugins = [{
-        name = "romkatv/powerlevel10k";
-        tags = [ "as:theme" "depth:1" ];
-      }];
-    };
+    #zplug = {
+    #  enable = true;
+    #  plugins = [{
+    #    name = "romkatv/powerlevel10k";
+    #    tags = [ "as:theme" "depth:1" ];
+    #  }];
+    #};
 
-    #plugins = [
-    #  {
-    #    name = "powerlevel10k";
-    #    src = pkgs.zsh-powerlevel10k;
-    #    file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    #  }
-    #  {
-    #    name = "powerlevel10k-config";
-    #    src = ./zsh;
-    #    file = "p10k.zsh";
-    #  }
-    #];
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./zsh;
+        file = ".p10k.zsh";
+      }
+    ];
   };
 
 }
