989d8b99080e0fd2fc5c3f19ebf206301cb08dad
Factoryidiot <rhys.scandlyn@gmail.com>
2024-10-05 14:53:18 +1000

    updated home/zsh.nix

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -94,26 +94,40 @@
     zsh = {
       enable = true;
       loginShellInit = "fastfetch";
-      shellInit = ''
-
-    if ! zmodload zsh/langinfo zsh/terminfo ||
-      [[ $langinfo[CODESET] != (utf|UTF)(-|)8 ||
-      $TERM == (dumb|linux) ]] ||
-      (( terminfo[colors] < 256 )); then
+  #    shellInit = ''
+#
+ #   if ! zmodload zsh/langinfo zsh/terminfo ||
+  #    [[ $langinfo[CODESET] != (utf|UTF)(-|)8 ||
+   #   $TERM == (dumb|linux) ]] ||
+    #  (( terminfo[colors] < 256 )); then
       # Don't use the powerline config. It won't work on this terminal.
  
       # Use 8 colors and ASCII.
       #source /usr/share/zsh/p10k-portable.zsh
-      source ~/.p10k-portable.zsh
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=black,bold'
-    else
+  #    source ~/.p10k-portable.zsh
+   #   ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=black,bold'
+   # else
       # Use 256 colors and UNICODE.
       #source /usr/share/zsh/p10k.zsh
-      source ~/.p10k.zsh
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
-    fi
+#      source ~/.p10k.zsh
+ #     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
+  #  fi
 
       '';
+
+      shellInit = ''
+        if [ "$TERM" = "linux" ]; then
+          # Use 256 colors and UNICODE.
+          #source /usr/share/zsh/p10k.zsh
+          source ~/.p10k.zsh
+          ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
+        else
+          # Use 8 colors and ASCII.
+          source ~/.p10k-portable.zsh
+          ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=black,bold'
+        fi
+      '';
+
 
       #shellInit = ''
       #  if [ "$TERM" = "linux" ]; then
