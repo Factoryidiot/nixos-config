9bcc9a2ecb689d947d93e06020dd37df3b692bcd
Factoryidiot <rhys.scandlyn@gmail.com>
2024-12-25 16:42:33 +1300

    update home/looking-glass-client.nix

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -95,29 +95,6 @@ in
   #};
   programs.hyprlock.enable = true;
 
-  programs.looking-glass-client = {
-    enable = true;
-    settings = {
-      app = {
-        allowDMA = true;
-        shmFile = "/dev/shm/looking-glass";
-      };
-      input = {
-        rawMouse = true;
-        escapeKey = "56";
-      };
-      spice = {
-        enable = true;
-        audio = true;
-      };
-      win = {
-        autoResize = true;
-        borderless = true;
-        quickSplash = true;
-      };
-    };
-  };
-
   services = {
     hypridle.enable = true;
     hyprpaper.enable = true;
