b15b2a7442dfa07b23023df8a97775df19e16a51
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2025-10-24 19:23:32 +1300

    nixos-config

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -1,119 +1,123 @@
 {
-  nixpkgs
-  , lib
-  , pkgs
-  , username
-  , ...
+  nixpkgs
+  , lib
+  , pkgs
+  , username
+  , ...
 }: {
 
-  # -------------------------------------------------------------------------
-  # NIX CONFIGURATION
-  # -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# NIX CONFIGURATION
+# -------------------------------------------------------------------------
 
-  nix = {
-    settings = {
-      accept-flake-config = true;
-      experimental-features = [ "nix-command" "flakes" ];
-      # Add the user to the trusted list for better performance
-      trusted-users = [ username "@wheel" ]; 
-      substituters = [
-        "https://cache.nixos.org"
-      ];
-    };
+  nix = {
+    settings = {
+      accept-flake-config = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      # Add the user to the trusted list for better performance
+      trusted-users = [ username "@wheel" ]; 
+      substituters = [
+        "https://cache.nixos.org"
+      ];
+    };
 
-    gc = {
-      automatic = lib.mkDefault true;
-      dates = lib.mkDefault "weekly";
-      options = lib.mkDefault "--delete-older-than 7d";
-    };
-  };
+    gc = {
+      automatic = lib.mkDefault true;
+      dates = lib.mkDefault "weekly";
+      options = lib.mkDefault "--delete-older-than 7d";
+    };
+  };
 
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-  # -------------------------------------------------------------------------
-  # CORE ENVIRONMENT
-  # -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# CORE ENVIRONMENT
+# -------------------------------------------------------------------------
+# Time and Locale settings moved to host-specific files
 
-  # Time and Locale settings moved to host-specific files
+  console  = {
+    keyMap = "us";
+  };
 
-  console  = {
-    keyMap = "us";
-  };
+  environment = {
+    systemPackages = with pkgs; [
+      clinfo
+      curl
+      git
+      lshw
+      pciutils
+      usbutils
+      vim
+      wget
+    ];
+    variables.EDITOR = "vim";
+  };
 
-  environment = {
-    systemPackages = with pkgs; [
-      clinfo
-      curl
-      git # required for flakes
-      lshw
-      pciutils
-      usbutils
-      vim
-      wget
-    ];
-    variables.EDITOR = "vim";
-  };
+# -------------------------------------------------------------------------
+# NETWORKING & SERVICES
+# -------------------------------------------------------------------------
 
-  # -------------------------------------------------------------------------
-  # NETWORKING & SERVICES
-  # -------------------------------------------------------------------------
+  networking = {
+    firewall.enable = true; # Universal default firewall setting
+    wireless.iwd.enable = true; # IWD is often a core networking tool
+    # The network manager choice (like networkmanager.enable) is left to the host file
+  };
 
-  networking = {
-    firewall.enable = true; # Universal default firewall setting
-    wireless.iwd.enable = true; # IWD is often a core networking tool
-    # The network manager choice (like networkmanager.enable) is left to the host file
-  };
+  programs = {
 
-  programs = {
-    gnupg.agent = {
-      enable = true;
-      enableBrowserSocket = true;
-      enableExtraSocket = true;
-      enableSSHSupport = true;
-    };
-    nano.enable = false;
-    zsh.enable = true;
-  };
+    gnupg.agent = {
+      enable = true;
+      enableBrowserSocket = true;
+       enableExtraSocket = true;
+        enableSSHSupport = true;
+    };
 
-  services = {
-    blueman.enable = true;
-    openssh = {
-      enable = true;
-      settings = {
-        X11Forwarding = true;
-        PermitRootLogin = "no";
-        PasswordAuthentication = false;
-      };
-      openFirewall = true;
-    };
+    nano.enable = false;
+    zsh.enable = true;
 
-    pipewire = {
-      enable = true;
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      jack.enable = true;
-      pulse.enable = true;
-      wireplumber.enable = true;
-    };
+  };
 
-    upower.enable = true;
-  };
+  services = {
+    blueman.enable = true;
 
-  # -------------------------------------------------------------------------
-  # USER & SHELL (COMMON DEFINITION)
-  # -------------------------------------------------------------------------
+    openssh = {
+      enable = true;
+      settings = {
+        X11Forwarding = true;
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+      };
+      openFirewall = true;
+    };
 
-  users = {
-    defaultUserShell = pkgs.zsh;
-    mutableUsers = true;
-    
-    # Define the core, universal properties of the user.
-    # Password and host-specific groups are defined in the host's file.
-    users.${username} = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ]; # All users need 'wheel' for sudo access
-    };
-  };
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      jack.enable = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+
+    upower.enable = true;
+  };
+
+# -------------------------------------------------------------------------
+# USER & SHELL (COMMON DEFINITION)
+# -------------------------------------------------------------------------
+
+  users = {
+    defaultUserShell = pkgs.zsh;
+    mutableUsers = true;
+
+    # Define the core, universal properties of the user.
+    # # Password and host-specific groups are defined in the host's file.
+    users.${username} = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # All users need 'wheel' for sudo access
+    };
+  };
+
 }
