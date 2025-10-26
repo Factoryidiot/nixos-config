94230741913e013364b155e31c153b6c9afae221
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2025-10-26 16:36:51 +1300

    nixos-install

--- nixos-install/flake.nix
+++ nixos-install/flake.nix
@@ -15,17 +15,12 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     preservation.url = "github:nix-community/preservation";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = inputs@{
     nixpkgs
-    , home-manager
     , preservation
     , self
     , ...
