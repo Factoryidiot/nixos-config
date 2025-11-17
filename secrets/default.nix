bfb484c27230a54cb30545df9d2dfce75b8276e1
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2025-11-17 21:22:49 +1300

    secrets

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,24 +1,14 @@
-{
-  agenix
-  , config
-  , pkgs
-  , secrets
-  , ...
-}:
-{
+# secrets/default.nix
+{ inputs, ... }: {
   imports = [
-    agenix.nixosModules.default
+    inputs.agenix.nixosModules.default
+    ./secrets.nix # This file will list all your secrets
   ];
 
+  # Tell agenix to use the host's existing SSH key for decryption
+  # This key is already persisted in /persistent/etc/ssh
   age.identityPaths = [
-    "/persistent/etc/ssh/ssh_host_ed25519_key"
+    "/etc/ssh/ssh_host_ed25519_key"
+    "/etc/ssh/ssh_host_rsa_key"
   ];
-
-  age.secrets = {
-    "ssh" = 
-    {
-    
-    }
-  };
-
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,20 @@
-let
-  rhys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBoXl6FEfhVy9TKbUbHyf1ipeiVpG4Wedw7yAxekMuK1";
-  users = [ rhys ];
-  whio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6Rh1ZZfh+mMI1ca/FIZSpQv/WGAKwNdvZQrHEP1w92";
-  systems  = [ whio ];
-in
-{
-  "secret01.age".publicKeys = [ users systems ];
-  "secret02.age".publicKeys = users ++ systems;
+# secrets/secrets.nix
+{ ... }: {
+  # This is where you define your secrets
+  age.secrets."my-test-secret" = {
+    # This file will be created in the next step
+    file = ./my-test-secret.age; 
+
+    # This specifies the list of *recipients* who can decrypt.
+    publicKeys = [
+      # 1. The public key of whio-test (from ssh-to-age)
+      "age1...[whio-test-public-key]..." 
+
+      # 2. Your user public key (from age-keygen in Step 1)
+      "age1...[rhys-public-key]..." 
+    ];
+  };
+
+  # By default, this secret will be decrypted and available at:
+  # /run/agenix/my-test-secret
 }
