318ae0e2bd707dc2009ba255b2a29850b34e67b9
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2025-11-19 22:00:48 +1300

    zsh

--- lib/home/zsh.nix
+++ lib/home/zsh.nix
@@ -9,23 +9,8 @@ let
 
 in
 {
-  #home.file = {
-    # Create the symlink for p10k.zsh
-    #"./p10k.zsh" = {
-    #  # The source path points to the existing file in the home directory,
-    #  # but we use mkOutOfStoreSymlink to tell Nix it's external.
-    #  source = ./zsh/p10k.zsh";
-    #  executable = true;
-    #};
 
-    # Create the symlink for p10k-portable.zsh
-    #"./p10k-portable.zsh" = {
-    #  source = ./zsh/p10k-portable.zsh";
-    #  executable = true;
-    #};
-  #};
-
-  # 2. Zsh Program Configuration
+  # Zsh Program Configuration
   programs.zsh = {
     enable = true;
     antidote = {
@@ -43,6 +28,9 @@ in
         "zshzoo/macos conditional:is-macos"
         "belak/zsh-utils path:utility"
         "romkatv/zsh-bench kind:path"
+
+        #oh-my-zsh
+        "getantidote/use-omz"
         "ohmyzsh/ohmyzsh path:plugins/extract"
 
         # Other Fish-like features
@@ -60,20 +48,7 @@ in
       ignoreAllDups = true;
     };
 
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "sudo" ];
-    };
-
-    #plugins = [
-    #  {
-    #    name = "powerlevel10k";
-    #    src = pkgs.zsh-powerlevel10k;
-    #    file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    #  }
-    #];
-
-    # 3. Zsh Initialisation Content
+    # Zsh Initialisation Content
     initContent = ''
       # Source P10K from the human-managed dotfiles directory
       if [ "$TERM" = "linux" ]; then
@@ -87,4 +62,5 @@ in
       # NOTE: We keep ZDOTDIR unset unless specifically needed for other configs.
     '';
   };
+
 }
