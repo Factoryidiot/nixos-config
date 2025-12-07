5711a399a78c73725176427281e351ce3019d363
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2025-12-07 14:50:11 +1300

    hyprland

--- lib/home/desktop/hyprland.nix
+++ lib/home/desktop/hyprland.nix
@@ -44,7 +44,9 @@ in
     "hypr/hyprlock.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hypr/hyprlock.conf";
     "hypr/hyprsunset.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hypr/hyprsunset.conf";
     "hypr/input.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hypr/input.conf";
+    "hypr/looknfeel.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hypr/looknfeel.conf";
     "hypr/monitors.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hypr/monitors.conf";
+    "hypr/windows.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hypr/windows.conf";
 
     # Wallpaper utility
     #"hypr/hyprpaper.conf".source = ./hypr/hyprpaper.conf;
