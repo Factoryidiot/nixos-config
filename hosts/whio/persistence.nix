ee6b92a6adb9c5d9b275692aadc557afc887d34d
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2025-12-27 08:39:20 +1300

    persistence

--- hosts/whio/persistence.nix
+++ hosts/whio/persistence.nix
@@ -1,113 +1,101 @@
 {
   impermanence
+  , specialArgs
   , ...
-}: {
+}:
+let
+  # Destructure 'username' from the specialArgs passed from flake.nix
+  inherit (specialArgs) username;
+in
+{
 
   imports = [
-    impermanence.nixosModules.impermanence
+    impermanence.nixosModules.default
   ];
 
-  # There are two ways to clear the root filesystem on every boot:
-  ##  1. use tmpfs for /
-  ##  2. (btrfs/zfs only)take a blank snapshot of the root filesystem and revert to it on every boot via:
-  ##     boot.initrd.postDeviceCommands = ''
-  ##       mkdir -p /run/mymount
-  ##       mount -o subvol=/ /dev/disk/by-uuid/UUID /run/mymount
-  ##       btrfs subvolume delete /run/mymount
-  ##       btrfs subvolume snapshot / /run/mymount
-  ##     '';
-  #
-  #  See also https://grahamc.com/blog/erase-your-darlings/
-
-  # NOTE: impermanence only mounts the directory/file list below to /persistent
-  # If the directory/file already exists in the root filesystem, you should
-  # move those files/directories to /persistent first!
   environment.persistence."/persistent" = {
-    # sets the mount option x-gvfs-hide on all the bind mounts
-    # to hide them from the file manager
     hideMounts = true;
+    # sets the mount option x-gvfs-hide on all the bind mounts
     directories = [
       "/etc/NetworkManager/system-connections"
       "/etc/ssh"
       "/etc/nix/inputs"
       "/etc/secureboot" # lanzaboote - secure boot
-      # my secrets
-      "/etc/agenix/"
+      "/etc/agenix/"    # secrets
 
-      "/var/log"
-      "/var/lib"
+      "/var/lib/nixos"
+      "/var/lib/flatpak"
 
-      # created by modules/nixos/misc/fhs-fonts.nix
-      # for flatpak apps
-      # "/usr/share/fonts"
-      # "/usr/share/icons"
+      # network
+      #"/var/lib/tailscale"
+      "/var/lib/bluetooth"
+      "/var/lib/NetworkManager"
+      "/var/lib/iwd"
     ];
+
     files = [
       "/etc/machine-id"
     ];
 
     # the following directories will be passed to /persistent/home/$USER
-    users.rhys = {
+    users.${username} = {
       directories = [
         "Documents"
         "Downloads"
         "Music"
         "Pictures"
+        "Videos"
+
+        ".dotfiles"
+
+        # Work
         "Projects/Nixos/nixos-config"
         "tmp"
-        "Videos"
 
         {
           directory = ".gnupg";
           mode = "0700";
         }
+
         {
           directory = ".ssh";
           mode = "0700";
         }
-
+	# Flatpak
+        ".var/app"
+        
         # misc
         ".config/pulse"
         ".pki"
-        ".steam" # steam games
+
+        # Games
+        ".steam"
+        ".local/share/Steam"
 
         # cloud native
-        #{
-        #  # pulumi - infrastructure as code
-        #  directory = ".pulumi";
-        #  mode = "0700";
-        #}
         {
           directory = ".aws";
           mode = "0700";
         }
-        #{
-        #  directory = ".docker";
-        #  mode = "0700";
-        #}
 
         # .config
-        ".config/Bitwarden"
-        ".config/google-chrome"
-        ".config/obsidian"
-        #".config/remmina"      # remote desktop
-        #".config/freerdp"      # remote desktop
+        #".config/Bitwarden"
+        #".config/google-chrome"
+        ".config/chromium"
+        #".config/obsidian"
 
-
-        # neovim / remmina / flatpak / ...
-        ".local/share"
+        ".local/share/flatpak"
         ".local/state"
 
         ".mozilla"
         # language package managers
         ".npm"
 
-        # neovim plugins(copilot)
-        #".config/github-copilot"
       ];
       files = [
-        ".zsh_history"
+        ".config/zsh/.zsh_history"
       ];
     };
   };
+
 }
