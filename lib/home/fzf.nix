bb1df1315955541a499d54d143c2d7360d56f238
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2025-12-30 19:11:49 +1300

    feat(fzf): enhance fzf configuration with zsh integration and fd
    This commit significantly enhances the fzf configuration.
    
    Key improvements include:
    - Enabling Zsh integration (`enableZshIntegration`) for automatic keybindings and completions.
    - Activating the change directory widget (`changeDirWidget`).
    - Setting `fd` as the default command for faster and more powerful searches, with `--hidden` and `--exclude .git`.
    - Adding various `extraOptions` for better UI/UX (ANSI colors, reverse layout, inline info, height).
    - Ensuring `fd` is installed as a dependency.

--- lib/home/fzf.nix
+++ lib/home/fzf.nix
@@ -1,8 +1,20 @@
+{ pkgs, ... }:
+
 {
-  pkgs
-  , ...
-}: {
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true; # Adds keybindings and completions
+    changeDirWidget = true; # Enables **fcd** (fzf change directory)
+    defaultCommand = "fd --type f --hidden --exclude .git"; # Use fd, exclude hidden files and .git
+    extraOptions = [
+      "--ansi" # Enable ANSI color codes
+      "--layout=reverse" # Show fzf window at the top
+      "--info=inline" # Show info line above the prompt
+      "--height=40%" # Set height of the fzf window
+    ];
+  };
 
-  programs.fzf.enable = true;
-
+  home.packages = with pkgs; [
+    fd # fzf needs fd for the defaultCommand
+  ];
 }
