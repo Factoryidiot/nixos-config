9d9bad9a5b51a352700617be1593dba198294dbc
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2026-04-26 10:27:35 +1200

    Secrets: Re-locking vault for Tahi

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,10 @@
 let
-  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJCkeOcvLsmdbtI/gkuqGSB5XQYLaLdF74M3Ck2vPuQ";
-  host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtXE792p3Vt7LGUOUmSsXwga73dGH3XoktIIqLAHInA";
+  factory = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJCkeOcvLsmdbtI/gkuqGSB5XQYLaLdF74M3Ck2vPuQ";
+  whio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtXE792p3Vt7LGUOUmSsXwga73dGH3XoktIIqLAHInA";
+  tahi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILks8XDjEE0EQgQIKx+tQbn+71LMX7RD2C3jOeTYztaA";
+  allKeys = [ factory whio tahi ];
 in
 {
-  "github.age".publicKeys = [ user host ];
-  "git-config.age".publicKeys = [ user host ];
+  "github.age".publicKeys = allKeys;
+  "git-config.age".publicKeys = allKeys;
 }
